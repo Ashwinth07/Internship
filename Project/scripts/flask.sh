be870087d5a7839b02d6c1d810d5dfd3b6410f85
Ashwinth07 <ashwinth023@gmail.com>
2023-12-08 13:40:15 +0530

    Scripts added

---
+++ Project/scripts/flask.sh
@@ -0,0 +1,5 @@
+pip install Flask
+python -m venv venv
+source venv/bin/activate
+pip freeze > requirements.txt
+pip install -r requirements.txt
---
+++ Project/scripts/postgres.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install -y postgresql
+pip install psycopg2-binary
+pip install flask_sqlalchemy
